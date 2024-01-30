eddd4ceb1f6f01d37676ed46a3966a3751b568c9
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2024-01-30 21:58:21 +0530

    Add Task Update

--- src/components/TaskFilter.jsx
+++ src/components/TaskFilter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function TaskFilter({ filter, onFilterChange }) {
   return (
-    <select value={filter} onChange={onFilterChange} className="w-full sm:w-64 h-10 rounded-md border border-black">
+    <select value={filter} onChange={onFilterChange} className="w-full sm:w-48 h-10 rounded-md border border-black bg-white text-black">
       <option value="All">All</option>
       <option value="Complete">Complete</option>
       <option value="Incomplete">Incomplete</option>
@@ -12,3 +12,4 @@ function TaskFilter({ filter, onFilterChange }) {
 }
 
 export default TaskFilter;
+
